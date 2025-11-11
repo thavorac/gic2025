6ebd16b435f5610b1337ab546b4bcd4ddab1e57a
Thavorac <thavorac.chun@gmail.com>
2025-11-11 10:02:52 +0700

    Some introduction code for TP1

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script>
+import CategoryComponent from './components/CategoryComponent.vue'
+const burgerImage = new URL('@/assets/burger.png', import.meta.url).href
+const peachImage = new URL('@/assets/peach.png', import.meta.url).href
+const kiwiImage = new URL('@/assets/kiwi.png', import.meta.url).href
+export default {
+  name: 'App',
+  data() {
+    return {
+      burgerImage,
+      peachImage,
+      kiwiImage,
+    }
+  },
+  components: {
+    CategoryComponent,
+  },
+}
+</script>
+
+<template>
+  <div class="category-wrapper">
+    <CategoryComponent title="Burget & Cake" :product-count="14" :image="burgerImage" />
+    <CategoryComponent title="Peach" :product-count="13" :image="peachImage" />
+    <CategoryComponent title="Organic Wiki" :product-count="15" :image="kiwiImage" />
+  </div>
+</template>
+
+<style scoped>
+.category-wrapper {
+  display: flex;
+  flex-direction: row;
+  gap: 15px;
+}
+</style>
---
+++ src/components/CategoryComponent.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="container">
+    <img :src="image" />
+    <h3>{{ title }}</h3>
+    <span class="count">{{ productCount }} items</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CategoryComponent',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    productCount: {
+      type: Number,
+      required: true,
+    },
+    image: {
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped>
+.container {
+  border: 1px solid #81b13d;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 10px;
+  background-color: #81b13d99;
+}
+.count {
+  color: #818080;
+  font-size: 12px;
+}
+</style>
